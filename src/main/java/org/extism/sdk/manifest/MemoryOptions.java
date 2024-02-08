91c19490d2d492566ba67fd223220d5ff35b6d64
zach <zach@dylibso.com>
2024-02-08 10:49:55 -0800

    feat: allow max HTTP response size to be set using the manifest (#19)

--- src/main/java/org/extism/sdk/manifest/MemoryOptions.java
+++ src/main/java/org/extism/sdk/manifest/MemoryOptions.java
@@ -6,13 +6,18 @@ import com.google.gson.annotations.SerializedName;
  * Configures memory for the Wasm runtime.
  * Memory is described in units of pages (64KB) and represent contiguous chunks of addressable memory.
  *
- * @param max Max number of pages.
+ * @param maxPages Max number of pages.
+ * @param httpMax Max number of bytes returned by HTTP requests using extism_http_request
  */
 public class MemoryOptions {
-    @SerializedName("max")
-    private final Integer max;
+    @SerializedName("max_pages")
+    private final Integer maxPages;
 
-    public MemoryOptions(Integer max) {
-        this.max = max;
+    @SerializedName("max_http_response_bytes")
+    private final Integer maxHttpResponseBytes;
+
+    public MemoryOptions(Integer maxPages, Integer httpMax) {
+        this.maxPages = maxPages;
+        this.maxHttpResponseBytes = httpMax;
     }
 }
