89a286ac0c50085f6640d9a57a19afe863f02041
Edoardo Vacchi <evacchi@users.noreply.github.com>
2024-09-05 19:53:42 +0200

    JNA callback should check inputs and outputs for null. (#30)
    This addresses https://github.com/extism/java-sdk/issues/27 once we
    merge https://github.com/extism/extism/pull/760 and release a new
    version of libextism.
    
    This PR simply check the parameters for null and the counts to be valid,
    in preparation for https://github.com/extism/extism/pull/760 which
    otherwise would cause a NullPointerException when outputs and inputs are
    empty.
    
    Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

--- src/main/java/org/extism/sdk/HostFunction.java
+++ src/main/java/org/extism/sdk/HostFunction.java
@@ -20,34 +20,12 @@ public class HostFunction<T extends HostUserData> {
 
     public final LibExtism.ExtismValType[] returns;
 
-    public final Optional<T> userData;
-
     public HostFunction(String name, LibExtism.ExtismValType[] params, LibExtism.ExtismValType[] returns, ExtismFunction f, Optional<T> userData) {
         this.freed = false;
         this.name = name;
         this.params = params;
         this.returns = returns;
-        this.userData = userData;
-        this.callback = (Pointer currentPlugin,
-                         LibExtism.ExtismVal inputs,
-                         int nInputs,
-                         LibExtism.ExtismVal outs,
-                         int nOutputs,
-                         Pointer data) -> {
-
-            LibExtism.ExtismVal[] outputs = (LibExtism.ExtismVal[]) outs.toArray(nOutputs);
-
-            f.invoke(
-                    new ExtismCurrentPlugin(currentPlugin),
-                    (LibExtism.ExtismVal[]) inputs.toArray(nInputs),
-                    outputs,
-                    userData
-            );
-
-            for (LibExtism.ExtismVal output : outputs) {
-                convertOutput(output, output);
-            }
-        };
+        this.callback = new Callback(f, userData);
 
         this.pointer = LibExtism.INSTANCE.extism_function_new(
                 this.name,
@@ -61,7 +39,7 @@ public class HostFunction<T extends HostUserData> {
         );
     }
 
-    void convertOutput(LibExtism.ExtismVal original, LibExtism.ExtismVal fromHostFunction) {
+    static void convertOutput(LibExtism.ExtismVal original, LibExtism.ExtismVal fromHostFunction) {
         if (fromHostFunction.t != original.t)
             throw new ExtismException(String.format("Output type mismatch, got %d but expected %d", fromHostFunction.t, original.t));
 
@@ -101,6 +79,47 @@ public class HostFunction<T extends HostUserData> {
         if (!this.freed) {
             LibExtism.INSTANCE.extism_function_free(this.pointer);
             this.freed = true;
+        }
+    }
+
+    static class Callback<T> implements LibExtism.InternalExtismFunction {
+        private final ExtismFunction f;
+        private final Optional<T> userData;
+
+        public Callback(ExtismFunction f, Optional<T> userData) {
+            this.f = f;
+            this.userData = userData;
+        }
+
+        @Override
+        public void invoke(Pointer currentPlugin, LibExtism.ExtismVal ins, int nInputs, LibExtism.ExtismVal outs, int nOutputs, Pointer data) {
+
+            LibExtism.ExtismVal[] inputs;
+            LibExtism.ExtismVal[] outputs;
+
+            if (outs == null) {
+                if (nOutputs > 0) {
+                    throw new ExtismException("Output array is null but nOutputs is greater than 0");
+                }
+                outputs = new LibExtism.ExtismVal[0];
+            } else {
+                outputs = (LibExtism.ExtismVal[]) outs.toArray(nOutputs);
+            }
+
+            if (ins == null) {
+                if (nInputs > 0) {
+                    throw new ExtismException("Input array is null but nInputs is greater than 0");
+                }
+                inputs = new LibExtism.ExtismVal[0];
+            } else {
+                inputs = (LibExtism.ExtismVal[]) ins.toArray(nInputs);
+            }
+
+            f.invoke(new ExtismCurrentPlugin(currentPlugin), inputs, outputs, userData);
+
+            for (LibExtism.ExtismVal output : outputs) {
+                convertOutput(output, output);
+            }
         }
     }
 }
